c4e99f2c4e71726dc45e6d384162e4bb9fb662e6
Alexandro T. Netto <alex.t.netto@gmail.com>
2024-07-13 23:51:38 +0200

    feat: get govtoken balances on hook

---
+++ src/DelegationMarketHook.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+import {Wrapper} from "./Wrapper.sol";
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+
+contract DelegationMarketHook is BaseHook, Wrapper {
+    uint256 oldBalance;
+    uint256 newBalance;
+
+    constructor(IPoolManager _poolManager, address _govToken) BaseHook(_poolManager) Wrapper(_govToken) {}
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: true,
+            afterRemoveLiquidity: true,
+            beforeSwap: false,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    function beforeRemoveLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata params,
+        bytes calldata hookData
+    ) external override returns (bytes4) {
+        oldBalance = govToken.balanceOf(sender);
+
+        return BaseHook.beforeRemoveLiquidity.selector;
+    }
+
+    function afterRemoveLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata params,
+        BalanceDelta delta,
+        bytes calldata hookData
+    ) external override returns (bytes4, BalanceDelta) {
+        newBalance = govToken.balanceOf(sender);
+
+        uint256 percentage = (oldBalance - newBalance) * 10_000 / oldBalance;
+
+        return (BaseHook.afterRemoveLiquidity.selector, delta);
+    }
+}
--- src/Wrapper.sol
+++ src/Wrapper.sol
@@ -2,23 +2,22 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract Wrapper is ERC20 {
     ERC20 govToken;
 
-    constructor(ERC20 _govToken)
-        ERC20(string.concat("Wrapped", _govToken.name()), string.concat("W", _govToken.symbol()))
+    constructor(address _govToken)
+        ERC20(string.concat("Wrapped", ERC20(_govToken).name()), string.concat("W", ERC20(_govToken).symbol()))
     {
-        govToken = _govToken;
+        govToken = ERC20(_govToken);
     }
 
-    function wrap(uint256 amount) public  {
+    function wrap(uint256 amount) public {
         govToken.transferFrom(msg.sender, address(this), amount);
         _mint(msg.sender, amount);
     }
 
-    function unwrap(uint256 amount) public  {
+    function unwrap(uint256 amount) public {
         _burn(msg.sender, amount);
         govToken.transferFrom(address(this), msg.sender, amount);
     }
