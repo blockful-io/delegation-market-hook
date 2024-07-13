17945578ffad300c4f2c804eb67a2565aba537f4
Alexandro T. Netto <alex.t.netto@gmail.com>
2024-07-13 18:44:24 +0200

    feat: GovToken

---
+++ src/GovToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20Votes} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {ERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Nonces} from "openzeppelin-contracts/contracts/utils/Nonces.sol";
+
+contract GovToken is ERC20, ERC20Votes, ERC20Permit {
+    constructor() ERC20("GovToken", "GOV") ERC20Permit("GovToken") {}
+
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
